91a0f478d289c39fd0d38d8025ad73ce9d77b10e
epiglottis <epiglottis.cartialge@gmail.com>
2025-10-24 22:17:01 +0800

    extract compute shader to separate file

--- src/main.rs
+++ src/main.rs
@@ -124,20 +124,7 @@ fn compute_pipeline() -> Result<()> {
     mod cs {
         vulkano_shaders::shader! {
             ty: "compute",
-            src: "
-                #version 460
-                
-                layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
-                
-                layout(set = 0, binding = 0) buffer Data {
-                    uint data[];
-                } buf;
-                
-                void main() {
-                    uint idx = gl_GlobalInvocationID.x;
-                    buf.data[idx] *= 114514;
-                }
-            "
+            path: "./shader/mul114514",
         }
     }
     let shader = cs::load(device.clone()).expect("failed to create shader module");
